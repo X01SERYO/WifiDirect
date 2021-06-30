fad8b96cf99d089a032d404fa1b44954b84d86e5
X01SERYO <sergio.s16a@hotmail.com>
2021-06-29 19:20:04 -0500

    update

---
+++ app/src/main/java/com/example/wifidirect/WiFiDirectBroadcastReceiver.kt
@@ -0,0 +1,11 @@
+package com.example.wifidirect
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class WiFiDirectBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
